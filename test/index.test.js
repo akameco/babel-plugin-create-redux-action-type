d571ce053e7ade05d3674e88a53ce17050a113e2
akameco <akameco.t@gmail.com>
2017-06-02 09:38:32 +0900

    Update test

--- test/index.test.js
+++ test/index.test.js
@@ -4,6 +4,7 @@ import plugin from '../src'
 
 function transformCode(input /* : string */, opts /* : ?Object */ = {}) {
   const { code } = transform(input, {
+    babelrc: false,
     plugins: [[plugin, opts]],
   })
   return code
@@ -11,53 +12,75 @@ function transformCode(input /* : string */, opts /* : ?Object */ = {}) {
 
 test('add flow comments', () => {
   const input = `
-    type HOGE_TYPE = 'hoge/HOGE_ACTION'
+type HOGE_TYPE = 'hoge/HOGE_ACTION'
   `
   expect(transformCode(input)).toMatchSnapshot()
 })
 
 test('add flow comments when exits flow comments', () => {
   const input = `
-    // @flow
-    type HOGE_TYPE = 'hoge/HOGE_ACTION'
-    type FUGA_TYPE = 'fuga/FUGA_ACTION'
+// @flow
+type HOGE_TYPE = 'hoge/HOGE_ACTION'
+type FUGA_TYPE = 'fuga/FUGA_ACTION'
   `
   expect(transformCode(input)).toMatchSnapshot()
 })
 
 test('add flow comments when exits flow block comments', () => {
   const input = `
-    /* @flow */
-    type HOGE_TYPE = 'hoge/HOGE_ACTION'
-    type FUGA_TYPE = 'fuga/FUGA_ACTION'
+/* @flow */
+type HOGE_TYPE = 'hoge/HOGE_ACTION'
+type FUGA_TYPE = 'fuga/FUGA_ACTION'
   `
   expect(transformCode(input)).toMatchSnapshot()
 })
 
 test('with extra', () => {
   const input = `
-    /* @flow */
-    type HOGE_TYPE = 'hoge/HOGE_ACTION'
-    type FUGA_TYPE = 'fuga/FUGA_ACTION'
+/* @flow */
+type HOGE_TYPE = 'hoge/HOGE_ACTION'
+type FUGA_TYPE = 'fuga/FUGA_ACTION'
 
-    const hoge = 'hoge'
-    const fuga = 'fuga'
+const hoge = 'hoge'
+const fuga = 'fuga'
 
-    type Action = { +type: HOGE_TYPE | HUGA_TYPE }
+type Action = { +type: HOGE_TYPE | HUGA_TYPE }
   `
   expect(transformCode(input)).toMatchSnapshot()
 })
 
 test('opts.filename', () => {
   const input = `
-    /* @flow */
-    type HOGE_TYPE = 'hoge/HOGE_ACTION'
-    type FUGA_TYPE = 'fuga/FUGA_ACTION'
+/* @flow */
+type HOGE_TYPE = 'hoge/HOGE_ACTION'
+type FUGA_TYPE = 'fuga/FUGA_ACTION'
 
-    const hoge = 'hoge'
-    const fuga = 'fuga'
+const hoge = 'hoge'
+const fuga = 'fuga'
 
-    type Action = { +type: HOGE_TYPE | HUGA_TYPE }
+type Action = { +type: HOGE_TYPE | HUGA_TYPE }
+  `
+  expect(transformCode(input, { filename: 'actionTypes' })).toMatchSnapshot()
+})
+
+test('export', () => {
+  const input = `
+/* @flow */
+export type HOGE_TYPE = 'hoge/HOGE_ACTION'
+export type FUGA_TYPE = 'fuga/FUGA_ACTION'
+
+type Action = { +type: HOGE_TYPE | HUGA_TYPE }
+  `
+  expect(transformCode(input, { filename: 'actionTypes' })).toMatchSnapshot()
+})
+
+test('real world', () => {
+  const input = `
+// @flow
+export type ADD_HOGE = 'Hoge/ADD_HOGE'
+export type EDIT_HOGE = 'Hoge/EDIT_HOGE'
+
+export type Action = { +type: ADD_HOGE | EDIT_HOGE }
   `
   expect(transformCode(input, { filename: 'actionTypes' })).toMatchSnapshot()
 })
